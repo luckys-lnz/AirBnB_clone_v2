19c22484b272f9d169261d4671a2778821dbafe9
deantosh daiddoh <deantoshdaiddoh@gmail.com>
2024-06-23 00:16:21 -0700

    command: do_create updated -- object creation with given parameters

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -118,13 +118,42 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
-            print("** class doesn't exist **")
-            return
-        new_instance = HBNBCommand.classes[args]()
+        else:
+            args_list = args.split()
+            if args_list[0] not in HBNBCommand.classes:
+                print("** class doesn't exist **")
+                return
+            if len(args_list) == 1:
+                new_instance = HBNBCommand.classes[args_list[0]]()
+            else:
+                # create an empty object
+                new_instance = HBNBCommand.classes[args_list[0]]()
+                obj_dict = new_instance.to_dict()
+                for item in args_list[1:]:
+                    try:
+                        attr_name, attr_value = item.split("=")
+
+                        # validate attribute value
+                        if attr_value[0] == '"' and attr_value[-1] == '"':
+                            # escape double quotes
+                            attr_value = attr_value[1:-1].replace('"', '\\"')
+                            # handle attr_value: replace underscores
+                            attr_name = attr_name.replace('_', ' ')
+                        elif '.' in attr_value:
+                            # convert to float
+                            attr_value = float(attr_value)
+                        else:
+                            # convert to int
+                            attr_value = int(attr_value)
+                    except (ValueError, IndexError):
+                        pass
+                    obj_dict[attr_name] = attr_value
+
+                # update object
+                new_instance = HBNBCommand.classes[args_list[0]](**obj_dict)
+
         storage.save()
         print(new_instance.id)
-        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
@@ -272,7 +301,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +309,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
@@ -319,6 +348,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
