57a8ea7178760f0c263a2821c2c698e31415287e
luckys-lnz <lalucky22@gmail.com>
2024-06-25 04:15:50 -0700

    modified for 9. DBStorage - Review

--- models/user.py
+++ models/user.py
@@ -16,3 +16,6 @@ class User(BaseModel, Base):
 
     places = relationship("Place", back_populates="user",
                           cascade="all, delete-orphan")
+
+    reviews = relationship("Review", back_populates="users",
+                           cascade="all, delete-orphan")
