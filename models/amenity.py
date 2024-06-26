2837faa9b29f5ad2f52ef414630126aaa7394446
luckys-lnz <lalucky22@gmail.com>
2024-06-26 12:23:17 -0700

    many to many relationship

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 import os
-from sqlalchemy import Column, String, ForeignKey
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
 
 
 # Get the storage type
