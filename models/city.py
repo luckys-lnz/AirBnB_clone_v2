0e1cf1e2b344c2bee3b5eac995b6ab6946cb16ca
deantosh daiddoh <deantoshdaiddoh@gmail.com>
2024-06-26 01:25:31 -0700

    Update: create table -- cities

--- models/city.py
+++ models/city.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+import os
+from sqlalchemy import Column, String, ForeignKey
+from models.base_model import BaseModel, Base
 
 
-class City(BaseModel):
+# Get storage type
+storage_type = os.getenv("HBNB_TYPE_STORAGE")
+
+
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+
+    if storage_type == "db":
+        # Handle DB storage
+        __tablename__ = 'cities'
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+    else:
+        # Handle File storage
+        state_id = ""
+        name = ""
