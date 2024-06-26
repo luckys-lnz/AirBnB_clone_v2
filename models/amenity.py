b0a1022d578c65f93d3959e892525cdbc18a62a9
deantosh daiddoh <deantoshdaiddoh@gmail.com>
2024-06-26 05:40:17 -0700

    Amenity class

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+import os
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
 
-class Amenity(BaseModel):
-    name = ""
+# Get the storage type
+storage_type = os.getenv("HBNB_TYPE_STORAGE")
+
+
+class Amenity(BaseModel, Base):
+    """ Defines Amenity class """
+
+    if storage_type == "db":
+        __tablename__ = 'amenities'
+        # Handle database storage
+        name = Column(String(128), nullable=False)
+
+        # Many-to-Many relationship
+        place_amenities = relationship('Place',
+                                       secondary='place_amenity',
+                                       viewonly=False)
+    else:
+        # Handle file storage
+        name = ""
