7b5ed0cd5eda064706a66ec434de700e3423fe87
deantosh <deantoshdaiddoh@gmail.com>
2024-07-25 10:09:15 -0700

    Simple web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module starts a Flask web applicatio and displays a 'Hello HBNBN'
+message.
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
