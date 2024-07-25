561dd6f3be79b323ec0c2206d2bfc3cf7f597520
deantosh <deantoshdaiddoh@gmail.com>
2024-07-25 12:36:22 -0700

    task 2: pass variable on route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Module starts a Flask web applicatio and displays a 'Hello HBNBN'
+message.
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_text(text):
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
