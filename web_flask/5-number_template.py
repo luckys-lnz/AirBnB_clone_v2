9fa4a69e560a95e1e3ea9e6c9ffc07badf54f1a2
deantosh <deantoshdaiddoh@gmail.com>
2024-07-26 09:52:43 -0700

    route: /number_template/<n>

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+Module starts a Flask web applicatio and displays a 'Hello HBNBN'
+message.
+"""
+from flask import Flask, abort, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_text(text):
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+@app.route('/python/')
+@app.route('/python/<text>', strict_slashes=False)
+def default(text='is cool'):
+    text = text.replace('_', ' ')
+    return f"Python {text}"
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def number(n):
+    try:
+        num = int(n)
+        return f"{n} is a number"
+    except ValueError:
+        abort(404)
+
+
+@app.route('/number_template/<n>', strict_slashes=False)
+def display_page(n):
+    try:
+        number = int(n)
+        return render_template('number.html', number=number)
+    except ValueError:
+        abort(404)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
