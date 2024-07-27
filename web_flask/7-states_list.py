d0c2558b9db7b0db31a63fb3089b4003b0b9ce8a
deantosh <deantoshdaiddoh@gmail.com>
2024-07-27 12:54:12 -0700

    Flask app -- list all states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Script starts a Flask web application:
+Requirements:
+ - Your web application must be listening on 0.0.0.0, port 5000
+ - You must use storage for fetching data from the storage engine
+   (FileStorage or DBStorage) => from models import storage and
+   storage.all(...)
+ - After each request you must remove the current SQLAlchemy Session:
+ - Declare a method to handle @app.teardown_appcontext
+ - Call in this method storage.close()
+ - Routes:
+      /states_list: display a HTML page: (inside the tag BODY)
+      H1 tag: “States”
+      UL tag: with the list of all State objects present in DBStorage
+      sorted by name (A->Z) tip
+         LI tag: description of one State: <state.id>: <B><state.name></B>
+ - Import this 7-dump to have some data
+ - You must use the option strict_slashes=False in your route definition
+"""
+from flask import Flask
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def delete_session(exception=None):
+    storage.close()
+
+
+@app.route('/state_list', strict_slashes=False)
+def list_states():
+    states = storage.all(State)
+    sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
