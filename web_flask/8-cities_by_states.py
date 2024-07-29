9fd32e9390605cda3c2b304f19d983339663eab7
deantosh <deantoshdaiddoh@gmail.com>
2024-07-29 07:35:39 -0700

    /states_list -- displays HTML page that lists all states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -2,24 +2,15 @@
 """
 Script starts a Flask web application:
 Requirements:
- - Your web application must be listening on 0.0.0.0, port 5000
- - You must use storage for fetching data from the storage engine
-   (FileStorage or DBStorage) => from models import storage and
-   storage.all(...)
- - After each request you must remove the current SQLAlchemy Session:
- - Declare a method to handle @app.teardown_appcontext
- - Call in this method storage.close()
  - Routes:
       /states_list: display a HTML page: (inside the tag BODY)
       H1 tag: “States”
       UL tag: with the list of all State objects present in DBStorage
       sorted by name (A->Z) tip
          LI tag: description of one State: <state.id>: <B><state.name></B>
- - Import this 7-dump to have some data
- - You must use the option strict_slashes=False in your route definition
 """
 from flask import Flask, render_template
-from models import storage
+from models import storage, storage_type
 from models.state import State
 
 
@@ -28,15 +19,18 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def delete_session(exception=None):
+    """ Closes the current session after each request """
     storage.close()
 
 
 @app.route('/cities_by_states', strict_slashes=False)
 def list_cities_states():
+    """ Displays a HTML that contains a list of all State objects """
     # Get records of the states
     states = storage.all(State).values()
-    return render_template('8-cities_by_states.html', states=states)
+    return render_template(
+        '8-cities_by_states.html', states=states, storage_type=storage_type)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
